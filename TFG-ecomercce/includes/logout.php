f3d24de39d5dfd57df60ee830fe95bf175bc9d45
David <secocuellard@gmail.com>
2025-05-20 17:39:47 +0200

    TFG

--- TFG-ecomercce/includes/logout.php
+++ TFG-ecomercce/includes/logout.php
@@ -2,5 +2,5 @@
 session_start();         // Inicia la sesión si aún no está iniciada
 session_unset();         // Elimina todas las variables de sesión
 session_destroy();       // Destruye la sesión
-header("Location: login.php"); // Redirige al login
+header("Location: ../pages/login.php"); // Redirige al login
 exit();
